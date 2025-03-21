10ec16a75e22194f0a316357eda5cc45847659ac
JulianGalloNexer <julian.gallo2@nexergroup.com>
2025-03-21 15:13:35 -0500

    config added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline text-light-500">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-inter">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </>
+  );
 }
