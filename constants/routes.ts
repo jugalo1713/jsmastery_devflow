267f00849f44ec95a05b10fc0b67d451c9b0373f
JulianGalloNexer <julian.gallo2@nexergroup.com>
2025-04-16 16:30:34 -0500

    right sidebar created

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,8 @@ const ROUTES = {
   HOME: "/",
   SIGNIN: "/sign-in",
   SIGNUP: "/sign-up",
+  PROFILE: (id: string) => `/profile/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
 };
 
 export default ROUTES;
