e1f92d3e5015e8342ca6ddaaf98130e33b54d888
JulianGalloNexer <julian.gallo2@nexergroup.com>
2025-05-22 11:55:26 -0500

    change to users and account routes

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,6 +2,7 @@ import User from "@/database/user.model";
 import { handleError } from "@/lib/handlers/error";
 import { NotFoundError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
 
@@ -18,6 +19,56 @@ export async function GET(
     if (!user) throw new NotFoundError("User");
 
     return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+
+    if (!id) throw new NotFoundError("User ID is required");
+
+    await dbConnect();
+
+    const users = await User.findByIdAndDelete(id);
+
+    if (!users) throw new NotFoundError("User not found");
+
+    return NextResponse.json({ success: true, data: users }, { status: 204 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+
+    if (!id) throw new NotFoundError("User ID is required");
+
+    await dbConnect();
+
+    const body = await request.json();
+    const validatedData = UserSchema.partial().parse(body);
+
+    const updatedUser = await User.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new NotFoundError("User not found");
+
+    return NextResponse.json(
+      { success: true, data: updatedUser },
+      { status: 200 }
+    );
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }
